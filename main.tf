03581b2fa93eba31d27aa10fcefa1c1d8dd2ae55
PC <rkiabouzid14@gmail.com>
2025-10-04 10:39:46 +0100

    hi34

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
+
 resource "aws_instance" "jenkins_server" {
-ami
-= "ami-0c55b159cbfafe1f0"
-instance_type = "t2.micro"
-key_name      = "key-test"
-tags = {
-Name = "Jenkins-Server"
-}
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = "key-test"
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
 }
