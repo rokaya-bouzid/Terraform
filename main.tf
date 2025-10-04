c8470d62d56ed95784b403ba6abe1ce725a58048
PC <rkiabouzid14@gmail.com>
2025-10-04 10:20:41 +0100

    hi34

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   tags = {
       Name = "TF-Instance"
   }
