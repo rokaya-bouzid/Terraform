12812f4ceefb6f335da489f91def2a4cb4bc2a6c
PC <rkiabouzid14@gmail.com>
2025-10-04 10:32:24 +0100

    hi34

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "jenkins_server" {
 ami
 = "ami-0c55b159cbfafe1f0"
 instance_type = "t2.micro"
-key_name      = "key_test"
+key_name      = "key-test"
 tags = {
 Name = "Jenkins-Server"
 }
