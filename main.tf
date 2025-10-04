ea7e4107256a97c632d45182ccaef834c3090f20
PC <rkiabouzid14@gmail.com>
2025-10-04 12:42:42 +0100

    modif7

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-north-1"
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami           = "ami-0a716d3f3b16d290c"
+  instance_type = "t3.micro"
   key_name      = "key-test"
 
   tags = {
